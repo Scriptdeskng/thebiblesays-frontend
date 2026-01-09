d24cbf43749f9280174de006f8eadcb7f3c2e282
Ayomide Jimoh <ayomidejimoh247@gmail.com>
2026-01-09 05:19:25 +0100

    feat: Display error message in login page

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -8,8 +8,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-      >
+      <body className="flex flex-col min-h-screen">
         <Header />
 
         <main className="flex-1">
